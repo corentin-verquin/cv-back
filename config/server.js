079c0ea58ca9186b8c0b4f95b3362d8335972ddb
corentin verquin <corentin.verquin@gmail.com>
2020-12-22 22:14:07 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '507f6c2c4b470a4881855c3670e267e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca5c9473-bbcf-4769-bcab-14602a72477c'
+};
