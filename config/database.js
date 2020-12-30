61751cb6cc4dcfd0d2fe6838bfd8de6bd78fbb54
corentin verquin <corentin.verquin@gmail.com>
2020-12-30 20:08:47 +0100

    update bdd config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL || 'postgres://psql:psql@127.0.0.1:5432/psql');
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -5,11 +8,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'psql'),
-        username: env('DATABASE_USERNAME', 'psql'),
-        password: env('DATABASE_PASSWORD', 'psql'),
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
